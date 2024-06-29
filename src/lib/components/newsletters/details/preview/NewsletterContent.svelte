d29ba2986a3fd2d95c700d02dd7010031afe1e6b
Frank <62514408+frank371ba@users.noreply.github.com>
2024-06-29 23:46:17 +0200

    fix: empty/partial NewsletterContent, if no/incomplete data

--- src/lib/components/newsletters/details/preview/NewsletterContent.svelte
+++ src/lib/components/newsletters/details/preview/NewsletterContent.svelte
@@ -7,6 +7,8 @@
 	export let newsletter;
 </script>
 
-<img alt="" class="" height="451" src="{newsletter.image}" width="451"/><br/>
-<br/>
-{@html marked.parse(newsletter.text)}
+{#if newsletter}
+    <img alt="" class="" height="451" src="{newsletter.image ?? ''}" width="451"/><br/>
+    <br/>
+    {@html marked.parse(newsletter.text ?? '')}
+{/if}
