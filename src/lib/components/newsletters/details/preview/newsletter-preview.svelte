6fc5cf908a8f4b7442ccc790128c4538a0873a52
Frank <62514408+frank371ba@users.noreply.github.com>
2024-06-29 22:45:56 +0200

    fix: an Änderungen von sveltia-cms angepasst

--- src/lib/components/newsletters/details/preview/newsletter-preview.svelte
+++ src/lib/components/newsletters/details/preview/newsletter-preview.svelte
@@ -1,21 +1,31 @@
 <script>
     import {entryDraft} from '$lib/services/contents/editor';
 
-    import NewsletterTemplate from "./NewsletterTemplate.svelte";
-    import NewsletterContent from "./NewsletterContent.svelte";
+    import NewsletterTemplate from './NewsletterTemplate.svelte';
+    import NewsletterContent from './NewsletterContent.svelte';
 
     /**
      * @type {LocaleCode}
      */
     export let locale;
 
-    $: ({currentValues} = $entryDraft);
-    $: newsletterValues = currentValues[locale];
-
-    const code = '';
-    const email = '';
+    let currentValues;
+    /**
+     * @type {Record<string, any>}
+     */
+    let newsletterValues;
+    $: if  ($entryDraft) {
+        ({currentValues} = $entryDraft);
+        newsletterValues = currentValues[locale] ?? [];
+    }
 </script>
 
-<NewsletterTemplate {code} {email}>
+<style>
+    :global(th, td) {
+        border: none;
+    }
+</style>
+
+<NewsletterTemplate>
     <NewsletterContent newsletter={newsletterValues}></NewsletterContent>
 </NewsletterTemplate>
