cee4efdd6ab10c4997cd1abfd0cb6e58fbfaa106
Frank <62514408+frank371ba@users.noreply.github.com>
2024-08-13 15:11:30 +0200

    feat: allow adding multiple texts and images to newsletters // generateBlobSrc if in preview, use src-string directly if sending Newsletter

--- src/lib/components/contents/details/toolbar.svelte
+++ src/lib/components/contents/details/toolbar.svelte
@@ -402,7 +402,7 @@
 
           /* eslint-disable */
           const elem = document.createElement('div');
-          new NewsletterContent({ target: elem, props: { newsletter } });
+          new NewsletterContent({ target: elem, props: { newsletter, generateBlobSrc: false } });
           const html = elem.innerHTML;
           /* eslint-enable */
 
