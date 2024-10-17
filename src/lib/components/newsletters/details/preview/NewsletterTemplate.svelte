94d307fed300e98e0c95e0c07074781b58b60dac
Frank <62514408+frankbits@users.noreply.github.com>
2024-10-17 15:36:56 +0200

    fix: "Zur Webansicht"-Link rote Text-Farbe

--- src/lib/components/newsletters/details/preview/NewsletterTemplate.svelte
+++ src/lib/components/newsletters/details/preview/NewsletterTemplate.svelte
@@ -611,7 +611,7 @@
 <svelte:body style="height: 100%;margin: 0;padding: 0;width: 100%;background-color: #333333;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;"></svelte:body>
 
 <center style="min-width: fit-content;">
-    <a href="/newsletter/{slug}">Zur Webansicht</a>
+    <a href="/newsletter/{slug}" style="color: #cc2944;">Zur Webansicht</a>
     <table align="center" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="bodyTable"
            style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;height: 100%;margin: 0;padding: 0;width: 100%;background-color: #333333;">
         <tr>
